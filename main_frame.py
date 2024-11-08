98aa069edfeb4ca856c2573259e39f9c9193cdca
SerumYouNeed <greg.miler@gmail.com>
2024-11-08 19:23:53 +0100

    working on switching frames

--- main_frame.py
+++ main_frame.py
@@ -8,10 +8,6 @@ class MainFrame(ctk.CTkFrame):
         self.grid(row=0, column=0, sticky="ewns")
 
     def create_widgets(self, parent):
-        # create the grid
-        self.rowconfigure((0,1,2), weight=1)
-        self.columnconfigure((0,1), weight=1)
-
         # create widgets
         self.next_frame = ProgrammingFrame1(self)
         grittings = ctk.CTkLabel(self, 
@@ -19,18 +15,19 @@ class MainFrame(ctk.CTkFrame):
                                  fg_color="black",
                                  text_color="white",
                                  font=("Helvatica", 27))
-        instructions = ctk.CTkTextbox(self, 
-                                 fg_color="black",
+        instructions = ctk.CTkLabel(self, 
                                  text_color="white",
                                  font=("Helvatica", 16),
-                                 wrap="word",
                                  bg_color="gray18",
-                                 corner_radius=8)
+                                 corner_radius=8, 
+                                 width=500,
+                                 wraplength=500,
+                                 justify="left")
         frecquency_q = ctk.CTkLabel(self, 
-                                    text="Training days:",
-                                    fg_color="black",
-                                    text_color="white",
-                                    font=("Helvatica", 22))
+                                 text="Training days:",
+                                 fg_color="black",
+                                 text_color="white",
+                                 font=("Helvatica", 22))
         next_btn = ctk.CTkButton(self,
                                  text="Next",
                                  font=("Helvatica", 15),
@@ -40,42 +37,34 @@ class MainFrame(ctk.CTkFrame):
                                  hover_color="gray18") 
                
         def combobox_callback(choice):
+            # instructions.delete()
             match choice:
                 case "1":
-                    instructions.insert("0.0", "One day a week is usually not enough - but hey, it is steel better that none. Pick max 7 exercises from dropdown menu below. Number of exercises is not random. That way you should cover all your major muscle groups. You can pick less and adjust later - but more will not give you better gains. One thing for sure: you will spent more than two hours in the gym meanwhile much of your sets will be counterproductive.")
-                    instructions.grid(row=2, column=0, columnspan=2)
-                    # parent.switch_frame(ProgrammingFrame1)
+                    instructions.configure(text="One day a week is usually not enough - but hey, it is still better that none.\nPick max 7 exercises from dropdown menu below. Number of exercises is not random.\nYou can pick less and adjust later.")
                 case "2":
-                    instructions.insert("0.0", "For two training days it is highly recomended to perform two full body training. In this case each muscle will be trained two times per week. You can stimulate muscles from diffrent angles - say, perform flat bench press on day one and incline on day two.")
-                    instructions.grid(row=2, column=0, columnspan=2)
-                    # parent.switch_frame(ProgrammingFrame2)
+                    instructions.configure(text="For two training days it is highly recomended to perform two full body training.\nIn this case each muscle will be trained two times per week.\nYou can stimulate muscles from diffrent angles - say, perform flat bench press on day one and incline on day two.")
+                #     # parent.switch_frame(ProgrammingFrame2)
                 case "3":
-                    instructions.insert("0.0", "Three days split can be aranged as a 3 x FBW, or you can create two routines A/B - like upper/lower, push/pull, torso/limbs, front/rear - and train A/B/A in first week, then B/A/B in second. Traing will be a tad longer then in four days per week but much more versatile then one and two days per week.")
-                    instructions.grid(row=2, column=0, columnspan=2)
+                    instructions.configure(text="Three days split can be aranged as 3 x FBW, or you can create two routines A/B - like upper/lower, push/pull, torso/limbs, front/rear - and train A/B/A in first week, then B/A/B in second.\nTraing will be a tad longer then in four days per week but much more versatile then one and two days per week.")
                 case "4":
-                    instructions.insert("0.0", "Four days split can be aranged as 4 x FBW, or you can create two routines A/B - like upper/lower, push/pull, torso/limbs, front/rear - and train A/B/A/B. Training each muscle twice a week is a sweet spot. You can add some volume and not stuck in the gym for hours.")
-                    instructions.grid(row=2, column=0, columnspan=2)
+                    instructions.configure(text="Four days split can be aranged as 4 x FBW, or you can create two routines A/B - like upper/lower, push/pull, torso/limbs, front/rear - and train A/B/A/B.\nTraining each muscle twice a week is a sweet spot. You can add some volume and not stuck in the gym for hours.")
                 case "5":
-                    instructions.insert("0.0", "Five days FBW split is hard to maintain, but if your recovery is ok you can try it. You can create routines like upper/lower, push/pull, torso/limbs, front/rear, push/pull/legs. Training each muscle more than twice a week can be really challenging.")
-                    instructions.grid(row=2, column=0, columnspan=2)
+                    instructions.configure(text="Five days FBW split is hard to maintain, but if your recovery is ok you can try it.\nYou can create routines like upper/lower, push/pull, torso/limbs, front/rear, push/pull/legs.\nTraining each muscle more than twice a week can be really challenging.")
                 case "6":
-                    instructions.insert("0.0", "Only if everything in your life is about GAINS. Remember that even pros do not workout everyday. It is really, really hard to maintain that kind of routine. Anyway, you will spend so much time in the gym that you can perform nearly every kind of workout.")
-                    instructions.grid(row=2, column=0, columnspan=2)
+                    instructions.configure(text="Only if everything in your life is about GAINS.\nRemember, even pros do not workout everyday.\nIt is really, really hard to maintain that kind of routine. Anyway, you will spend so much time in the gym that you can perform nearly every kind of workout.")
                 case "7":
-                    instructions.insert("0.0", "Really...? Give me a break.")
-                    instructions.grid(row=2, column=0, columnspan=2)
+                    instructions.configure(text="Really...? Do you REALLY want to go to the gym every day? Give me a break.")
+            instructions.pack(pady=30, padx=30, ipady=15, ipadx=15, fill="both")
 
         frecquency_selector = ctk.CTkComboBox(self, 
                                               values=["1", "2", "3", "4", "5", "6" , "7"], 
                                               command=combobox_callback)
 
         # place widgets
-        # Sticky pin frame to east, west, north, south 
-        grittings.grid(row=0, column=0, columnspan=2)
-        frecquency_q.grid(row=1, column=0, sticky="e")
-        frecquency_selector.grid(row=1, column=1, sticky="w")
-        next_btn.grid(row=3, column=0, columnspan=2)
-
+        grittings.pack(pady=50)
+        frecquency_q.pack(pady=15)
+        frecquency_selector.pack(pady=15)
+        next_btn.pack()
 
 
 
