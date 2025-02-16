bf30e7db23f706803405d3e65d6dbe00e93296eb
SerumYouNeed <greg.miler@gmail.com>
2025-02-16 23:56:07 +0100

    save pdf

--- save_btn.py
+++ save_btn.py
@@ -7,14 +7,12 @@ class SaveButton(ctk.CTkButton):
         super().__init__(master)
         self.plan = master.daily_routines
 
+        # Nonactive function. Create csv version of a plan.
         def create_csv_plan():
-
             with open('workout_plan.csv', mode='w') as csv_file:
-                fieldnames = ['day', 'exercise', 'sets']
-                
+                fieldnames = ['day', 'exercise', 'sets']    
                 writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
                 writer.writeheader()
-
                 for i in self.plan.keys():
                     writer.writerow({'day':i})
                     for k, v in self.plan[i].items():
@@ -23,7 +21,10 @@ class SaveButton(ctk.CTkButton):
         def create_pdf():
             # A4 pagesize
             c = canvas.Canvas("workout_plan.pdf", pagesize=(595.27, 841.89)) 
-            c.drawString(50, 780, "Hello Again")
+            for i in self.plan.keys():
+                c.drawString(50, 780, i)    
+                for k, v in self.plan[i].items():
+                    c.drawString(50, 780, f'{k}: {v}')
             # finnish page
             c.showPage()
             # save to pdf
